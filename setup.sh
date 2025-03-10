6a1e571970af3b085358b2ba9c1307e013321986
Soroush Mirzaee <72804931+DeltaTheM2@users.noreply.github.com>
2025-03-10 00:24:18 -0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -5,25 +5,25 @@ PROJECT_DIR="/home/jerry/TrackTrail-hardware"
 SERVICE_DIR="/etc/systemd/system"
 
 # Copy service files to systemd directory
-sudo cp "$PROJECT_DIR/helmet.service" "$SERVICE_DIR/"
-sudo cp "$PROJECT_DIR/gps.service" "$SERVICE_DIR/"
-sudo cp "$PROJECT_DIR/main.service" "$SERVICE_DIR/"
+cp "$PROJECT_DIR/helmet.service" "$SERVICE_DIR/"
+cp "$PROJECT_DIR/gps.service" "$SERVICE_DIR/"
+cp "$PROJECT_DIR/main.service" "$SERVICE_DIR/"
 
 # Reload systemd to recognize new service files
-sudo systemctl daemon-reload
+systemctl daemon-reload
 
 # Enable services to start on boot
-sudo systemctl enable helmet.service
-sudo systemctl enable gps.service
-sudo systemctl enable main.service
+systemctl enable helmet.service
+systemctl enable gps.service
+systemctl enable main.service
 
 # Start services immediately
-sudo systemctl start helmet.service
-sudo systemctl start gps.service
-sudo systemctl start main.service
+systemctl start helmet.service
+systemctl start gps.service
+systemctl start main.service
 
 # Optional: Check status of services
 echo "Checking service status..."
-sudo systemctl status helmet.service
-sudo systemctl status gps.service
-sudo systemctl status main.service
+systemctl status helmet.service
+systemctl status gps.service
+systemctl status main.service
